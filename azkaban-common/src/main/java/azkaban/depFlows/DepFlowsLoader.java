16e13a3a74c2704181cc0b94b40029e0931805e0
10890 <zhilong.deng@hand-china.com>
2016-10-21 11:10:43 +0800

    增加计划流的互斥与依赖

--- azkaban-common/src/main/java/azkaban/depFlows/DepFlowsLoader.java
+++ azkaban-common/src/main/java/azkaban/depFlows/DepFlowsLoader.java
@@ -6,6 +6,6 @@ import java.util.List;
  * Created by 邓志龙 on 2016/10/19.
  */
 public interface DepFlowsLoader {
-    public List<DepFlows> loadDepFlows(String flow_id);
+    public List<DepFlows> loadDepFlows(String flow_id,Integer project_id);
     //public int checkMutFlows(String flow_id);
 }
--- azkaban-common/src/main/java/azkaban/mutFlows/MutFlowsLoader.java
+++ azkaban-common/src/main/java/azkaban/mutFlows/MutFlowsLoader.java
@@ -6,5 +6,5 @@ import java.util.List;
  * Created by 邓志龙 on 2016/10/19.
  */
 public interface MutFlowsLoader {
-    public List<MutFlows> loadMutFlows(String flow_id);
+    public List<MutFlows> loadMutFlows(String flow_id,Integer project_id);
 }
